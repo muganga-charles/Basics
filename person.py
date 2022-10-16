a7296f0860d440adb20d7a6c6de8352c89d2ecec
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-16 20:01:00 +0300

    Update person.py

--- person.py
+++ person.py
@@ -4,12 +4,14 @@
 # The python language is serious when in comes to indention and hence the format of the file should seriously be followed
 # this is a function and it is going to be used to call out certain values
 # This program is going to be used to prompt  a user to enter their name and country and age and  city
+# This is the main function used to call values from the inner function
 def main():
+   
    
    name,country = get_person()
    print(f"Name is{name} country{country}")
    
-    
+#   This is an inner function  used by the ser to input values   
 def get_person():
     name= input("What is your name?")
     age =int(input("'What is your name?"))
