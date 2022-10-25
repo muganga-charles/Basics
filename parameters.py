91542576d717f9a593af4e8d07b1194cd7257618
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-25 11:25:59 +0300

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,9 @@
+def hello_user(name):
+    print('hello friend',name)
+    print('Hello Love',name)
+    print('I miss you',name)
+    
+
+hello_user('Adam')
+hello_user('Mom')
+hello_user('Ruth')   
